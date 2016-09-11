cfc3dff642768f32db448cb86f29c23b663a6ddf
kdinh9 <merikakun@gmail.com>
2016-09-12 03:53:12 +0700

    dasdas

--- JenkinsTest/Controllers/DefaultController.cs
+++ JenkinsTest/Controllers/DefaultController.cs
@@ -8,7 +8,7 @@ namespace JenkinsTest.Controllers
 {
     public class DefaultController : Controller
     {
-        // GET: Default/
+        // GET: Default/ /
         public ActionResult Index()
         {
             return View();
